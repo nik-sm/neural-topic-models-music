f1634f948cd91f2e679ebab5fc9b08b3df5363a8
Niklas Smedemark-Margulies <niklas.sm+github@gmail.com>
2019-04-16 23:53:50 -0400

    change preprocessing: remove empty songs, down/upsample to exactly 10K songs per genre, subset genres, better generator code, save song labels for supervised tasks.
    runs in 30m on full 'lyrics.csv'

--- notebooks/cleanData.py
+++ notebooks/cleanData.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 from collections import defaultdict, Counter
-
 import string
-
 import nltk
 nltk_path = './nltk_packages'
 # NLTK needs to download several packages
@@ -44,49 +41,76 @@ def vectorize(doc):
 def init_gen(n_songs, df, stop, stem):
     if n_songs != None:
         print("\tOnly using first {} songs".format(n_songs))
-        return (stemAndCount(song=song, stopwordsSet=stop, stemmer=stem) for song in df["lyrics"].head(n_songs))
+        for index, song in df[["index","lyrics"]].head(n_songs).iterrows():
+            yield (index, stemAndCount(song=song["lyrics"], stopwordsSet=stop, stemmer=stem)) 
     else:
         print("\tUsing all songs")
-        return (stemAndCount(song=song, stopwordsSet=stop, stemmer=stem) for song in df["lyrics"])
+        for index, song in df[["index","lyrics"]].iterrows():
+            yield (index, stemAndCount(song=song["lyrics"], stopwordsSet=stop, stemmer=stem)) 
 
 def main(n_songs=None):
     print("Begin data cleaning")
 
-    infile = "../data/lyrics_clean.pickle"
+    infile = "../data/lyrics.csv"
     print("Input data file: {}".format(infile))
-    df = pd.read_pickle(infile)
+    df = pd.read_csv(infile)
+
+    print("Step 0: Drop songs without lyrics. Subset by genre, and down/upsample")
+    chosen_genres = ['Rock','R&B','Pop','Metal','Jazz','Indie','Hip-Hop','Folk','Electronic','Country']
+    df = df[(df.genre.isin(chosen_genres))].dropna(0, subset=['lyrics'])
+
+    n_sample=10000
+    seed=0
+    by_genre = []
+    for g in chosen_genres:
+        by_genre.append(df[(df.genre == g)].sample(n=n_sample,replace=True,random_state=seed))
+    df = pd.concat(by_genre)
 
     print("Step 1: Stopping and Stemming")
     stemmer = PorterStemmer()
     stopwordsSet = stopwords.words("english")
 
     print("Step 2: Add all the counters and identify the top K words")
-    counters = init_gen(n_songs, df, stopwordsSet, stemmer)
     total_count = Counter()
-    i = 0
-    for c in counters:
-        i += 1
-        if (i % 1000 == 0):
-            print("\titeration", i)
+    iteration = 0
+    for idx, c in init_gen(n_songs, df, stopwordsSet, stemmer):
+        iteration += 1
+        if (iteration % 1000 == 0):
+            print("\titeration", iteration)
         total_count += c
 
     # Keep only the keys
     top_K = [k for k,v in total_count.most_common(2000)]
 
     # NOTE - need to reset the generator because we already traversed it once
-    counters = init_gen(n_songs, df, stopwordsSet, stemmer)
+    print("Step 3: Convert each song to bag-of-words representation")
     top_K_counts = []
-    for c in counters:
+    iteration = 0
+    for idx, c in init_gen(n_songs, df, stopwordsSet, stemmer):
+        iteration += 1
+        if (iteration % 1000 == 0):
+            print("\titeration", iteration)
         # lookup on an unused key just returns 0, like we want
-        top_K_counts.append([c[item] for item in top_K])
+        this_song = []
+        this_song.append(idx)
+        this_song.extend([c[item] for item in top_K])
+        top_K_counts.append(this_song)
 
-    print("Step 3: Convert to DataFrame")
+    print("Step 4: Convert to DataFrame")
     sampleFrame = pd.DataFrame(top_K_counts)
-    sampleFrame.columns = top_K # Fix column headers (TODO is this a safe approach?)
+    column_headers = []
+    column_headers.append("index")
+    column_headers.extend(top_K)
+    sampleFrame.columns = column_headers # Fix column headers (TODO is this a safe approach?)
 
     print("\tSave DF to pickle...")
-    sampleFrame.to_pickle("../data/bagOfWords.pickle")
+    sampleFrame.to_pickle("../data/bag_of_words.pickle")
+
+    print("\tSave all labels for song_ids...")
+    df[(df.index.isin(sampleFrame['index']))][['index','song','year','artist','genre']].to_pickle("../data/song_labels.pickle")
 
 if __name__ == '__main__':
-    #main()
-    main(n_songs=10000)
+    main()
+    # NOTE the operation of taking only 100 happens after upsampling, so then the songs can be duplicated and the final "labels" file can have more rows than the data file.
+    # So treat this n_songs as for debugging purposes only
+    #main(n_songs=100)
