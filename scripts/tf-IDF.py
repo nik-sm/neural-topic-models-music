4c786385f0ebf24dfcc133aee6506cb4673e75fc
Dzeiberg <dzeiberg@me.com>
2019-04-18 19:18:21 -0400

    standardize tf-idf

--- scripts/tf-IDF.py
+++ scripts/tf-IDF.py
@@ -11,12 +11,13 @@ import numpy as np
 from helpers import *
 import os
 from pipeline import newPipe
+from sklearn.preprocessing import StandardScaler
 
 ## Train on Song Corpus
 
 # In[17]:
 
-hParams = {"mode":"tf-idf", "max_features":100}
+hParams = {"mode":"tf-idf", "max_features":2000}
 corpus = np.load("../data/corpus.npy")
 leGenres = np.load("../data/leGenres.npy")
 
@@ -32,12 +33,14 @@ songStringCorpus = [" ".join(song) for song in corpus]
 
 songVectorizer = TfidfVectorizer(stop_words="english", max_features=hParams["max_features"])
 songTFIDF = songVectorizer.fit_transform(songStringCorpus)
+tfScaler = StandardScaler()
+songTFIDF = tfScaler.fit_transform(songTFIDF)
 
 
 # In[ ]:
 
 expPath = makeExpDir()
-expDict = newPipe(songTFIDF, leGenres)
+expDict = newPipe(songTFIDF, leGenres, iters=1)
 np.save(os.path.join(expPath, "LogisticRegressionDict.npy"), expDict)
 with open(os.path.join(expPath, "hParams.txt"), "w") as f:
     for k,v in hParams:
