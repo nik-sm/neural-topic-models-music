77f838d19d7c7d2d3c6a9a2d28f6dba2c6e25abb
Dzeiberg <dzeiberg@me.com>
2019-04-18 16:48:17 -0400

    fix conflict

--- scripts/makeCorpusAndGenres.py
+++ scripts/makeCorpusAndGenres.py
@@ -2,21 +2,23 @@ import numpy as np
 import pandas as pd
 from joblib import Parallel, delayed
 import gensim.utils as gu
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 print("reading lyrics csv")
 df = pd.read_csv("../data/lyrics.csv").dropna(0, subset=["lyrics","genre"])
 
-badWords = ["verse", "chorus"]
+
 def doWork(song, songIDX):
     if not songIDX%100:
         print("song number", songIDX)
     if not pd.isnull(song):
         tokens = [w for w in gu.simple_preprocess(song) if w not in badWords]
         if len(tokens):
-            return [w for w in gu.simple_preprocess(song) if w not in badWords], songIDX
+            return tokens, songIDX
+
+# PREPARE Word list Corpus and Genres
 print("doing work on songs")
 corpusAndGenres = Parallel(n_jobs=4)(delayed(doWork)(song, i) for i, song in enumerate(df["lyrics"]))
-
 print("separating corpus")
 corpus = [t[0] for t in corpusAndGenres]
 print("separating genres")
@@ -27,3 +29,8 @@ print("saving files")
 np.save("../data/corpus.npy")
 np.save("../data/genres.npy")
 np.save("../data/leGenres.npy")
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
