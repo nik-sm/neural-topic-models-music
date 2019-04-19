4831bcea78182123b5838e313cd260c3901b9eae
Dzeiberg <dzeiberg@me.com>
2019-04-18 20:05:50 -0400

    ramp up max_iters in log reg

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -62,16 +62,16 @@ def newPipe(features, labels, iters=10, regularization="l2", multi_class="multin
             for cNum, c in enumerate(paramGrid["C"]):
                 if multi_class=="multinomial":
                     if regularization == "l2":
-                        logReg = LogisticRegression(penalty="l2", class_weight='balanced', C=c, solver='lbfgs',multi_class=multi_class)
+                        logReg = LogisticRegression(max_iter=1000, penalty="l2", class_weight='balanced', C=c, solver='lbfgs',multi_class=multi_class)
                     elif regularization == "l1":
-                        logReg = LogisticRegression(penalty="l1", class_weight='balanced', C=c, solver='lbfgs',multi_class=multi_class)
+                        logReg = LogisticRegression(max_iter=1000, penalty="l1", class_weight='balanced', C=c, solver='lbfgs',multi_class=multi_class)
                     else:
                         assert False, "{} regularization is not supported".format(regularization)
                 elif multi_class=="ovr":
                     if regularization == "l2":
-                        logReg = LogisticRegression(penalty="l2", class_weight='balanced', C=c,multi_class=multi_class)
+                        logReg = LogisticRegression(max_iter=1000, penalty="l2", class_weight='balanced', C=c,multi_class=multi_class)
                     elif regularization == "l1":
-                        logReg = LogisticRegression(penalty="l1", class_weight='balanced', C=c,multi_class=multi_class)
+                        logReg = LogisticRegression(max_iter=1000, penalty="l1", class_weight='balanced', C=c,multi_class=multi_class)
                     else:
                         assert False, "{} regularization is not supported".format(regularization)
                 logReg.fit(xTrain, yTrain)
