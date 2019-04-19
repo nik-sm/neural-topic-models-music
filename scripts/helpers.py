02cac9777633f9fc561371d7cf9deb3c29f7d514
Dzeiberg <dzeiberg@me.com>
2019-04-18 22:35:26 -0400

    scripts to train word2Vec and doc2Vec using nik features

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -5,3 +5,16 @@ def makeExpDir():
     expPath = os.path.join("../results/experiments/",strftime("%Y_%m_%d_%H_%M_%S",gmtime()))
     os.mkdir(expPath)
     return expPath
+
+class EpochLogger(CallbackAny2Vec):
+'''Callback to log information about training'''
+
+def __init__(self):
+    self.epoch = 0
+
+def on_epoch_begin(self, model):
+    print("Epoch #{} start".format(self.epoch))
+
+def on_epoch_end(self, model):
+    print("Epoch #{} end".format(self.epoch))
+    self.epoch += 1
