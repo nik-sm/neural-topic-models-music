3279d6da9e1c09c1173c551e77fa39fd0c87325c
Dzeiberg <dzeiberg@me.com>
2019-04-19 12:15:23 -0400

    updated tfidf script

--- scripts/tf-IDF.py
+++ scripts/tf-IDF.py
@@ -1,46 +1,43 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import LabelEncoder
-from pipeline import newPipe
 import numpy as np
-from helpers import *
 import os
 from pipeline import newPipe
-from sklearn.preprocessing import StandardScaler
+from helpers import *
+from joblib import Parallel, delayed
 from sklearn.utils import resample
 
-hParams = {"mode":"tf-idf",
-"max_features":2000,
-"multi_class":"ovr",
-"resample":"all",
-"iters":10}
+hParams = {"iters":10,
+"max_iter":4000,
+"regularization":"l2",
+"multi_class":"multinomial",
+"subsample":1000
+}
 
-fullCorpus = np.load("../data/nik/listCorpus.npy")
-fullGenres = np.load("../data/nik/genre.npy")
-
-if hParams["resample"]=="all":
-    corpus=fullCorpus
-    leGenres = fullGenres
+allfeatures = np.load("../data/nik/tfidf.npy").item()
+allgenres = np.load("../data/nik/genre.npy")
+if hParams["subsample"]=="all":
+    features = allfeatures
+    genres = allgenres
 else:
-    corpus, leGenres = resample(fullCorpus,
-        fullGenres,
+    features, genres = resample(allfeatures,
+        allgenres,
         replace=False,
-        n_samples=hParams["resample"],
-        )
+        n_samples=hParams["subsample"])
 
-songStringCorpus = [" ".join(song) for song in corpus]
+# In[6]:
+def runExp(features):
+    expPath = makeExpDir()
 
+    experimentDict = newPipe(features,
+        genres,
+        iters=hParams["iters"],
+        max_iter=hParams["max_iter"],
+        regularization=hParams["regularization"],
+        multi_class=hParams["multi_class"])
 
+    np.save(os.path.join(expPath, "LogisticRegressionDict.npy"), experimentDict)
+    np.save(os.path.join(expPath, "hParams.npy"), hParams)
+    with open(os.path.join(expPath, "hParams.txt"), "w") as f:
+        for k,v in hParams:
+            f.write("{}:{}\n".format(k,v))
 
-songVectorizer = TfidfVectorizer(stop_words="english",
-    max_features=hParams["max_features"])
-
-songTFIDF = songVectorizer.fit_transform(songStringCorpus)
-tfScaler = StandardScaler(with_mean=False)
-songTFIDF = tfScaler.fit_transform(songTFIDF)
-
-expPath = makeExpDir()
-expDict = newPipe(songTFIDF, leGenres, iters=hParams["iters"], multi_class=hParams["multi_class"])
-np.save(os.path.join(expPath, "LogisticRegressionDict.npy"), expDict)
-with open(os.path.join(expPath, "hParams.txt"), "w") as f:
-    for k,v in hParams.items():
-        f.write("{}:{}\n".format(k,v))
+runExp(features)
